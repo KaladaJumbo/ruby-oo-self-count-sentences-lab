a0677f457af5410f72d4f2f4dacc4511229ae883
KaladaJumbo <wkalada1@gmail.com>
2020-08-27 01:35:22 -0400

    final commit

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,17 +4,46 @@ class String
 
   def sentence?
     
+    if self[-1] == "."
+      return true 
+    else 
+      return false
+    end 
+
   end
 
   def question?
+
+    if self[-1] == "?"
+      return true 
+    else 
+      return false
+    end 
 
   end
 
   def exclamation?
 
+    if self[-1] == "!"
+      return true 
+    else 
+      return false
+    end 
+
   end
 
   def count_sentences
+
+    sentence_count = 0
+    sentences_array = self.split(/[.?!]/)
+    sentences_array.each do |element|
+      if element != ""
+        sentence_count +=1
+      end
+
+    end
+    
+    return sentence_count
 
   end
 end
